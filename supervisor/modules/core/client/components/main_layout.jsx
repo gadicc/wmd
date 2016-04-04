9397c21323e77fcbaf2961e7149b448cf7d27ea1
Gadi Cohen <dragon@wastelands.net>
2016-04-04 09:24:04 +0200

    pure / cache tabs in mainlayout

--- supervisor/modules/core/client/components/main_layout.jsx
+++ supervisor/modules/core/client/components/main_layout.jsx
@@ -4,27 +4,40 @@ import { Tab, Tabs } from 'react-toolbox/lib/tabs';
 import { tabs } from 'meteor/wmd-extensions';
 import Blaze from 'meteor/gadicc:blaze-react-component';
 import { Provider } from 'react-redux'
+import pure from 'recompose/pure';
 
-const Layout = ({tab, actions, context}) => {
-  const { Store } = context();
-  const onChange = actions().mainLayout.onChange;
+const StoreProvider = pure((props) => {
+  const { Store } = props.context();
   return (
     <Provider store={Store}>
-      <div>
-        <div style={{ float: 'right' }}>
-          <Blaze template="loginButtons" align="right" />
-        </div>
-        <div>
-          <Tabs tab={tab} onChange={onChange}>
-           { tabs.map(({key, name, content}) => {
-              content = React.createElement(content); // if...
-              return (<Tab key={key} label={name}>{content}</Tab> );
-            }) }
-          </Tabs>    
-        </div>
-      </div>
+      <MainLayout {...props} />
     </Provider>
   );
-};
+});
 
-export default Layout;
+var _cache = {};
+function fromCache(content) {
+  if (!_cache[content])
+    _cache[content] = React.createElement(content);
+  return _cache[content];
+}
+
+const MainLayout = pure(({tab, actions}) => {
+  const onChange = actions().mainLayout.onChange;
+  return (
+    <div>
+      <div style={{ float: 'right' }}>
+        <Blaze template="loginButtons" align="right" />
+      </div>
+      <div>
+        <Tabs tab={tab} onChange={onChange}>
+         { tabs.map(({key, name, content}) => (
+            <Tab key={key} label={name}>{fromCache(content)}</Tab>
+         )) }
+        </Tabs>    
+      </div>
+    </div>
+  );
+});
+
+export default StoreProvider;
